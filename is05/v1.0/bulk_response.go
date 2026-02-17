28a715f432ca60863720ee596f2667a8aee9f1b9
cassaram <michael@cassara.tech>
2026-02-17 11:52:56 -0500

    IS-05 - Conform file naming

--- is05/v1.0/bulk_response.go
+++ is05/v1.0/bulk_response.go
@@ -7,5 +7,5 @@ type BulkResponse struct {
 	ID    string      `json:"id"`              // ID of a device to be activated
 	Code  int         `json:"code"`            // HTTP status code that would have resulted from an individual activation on this device
 	Error string      `json:"error,omitempty"` // Human readable message which is suitable for user interface display, and helpful to the user. Only included if 'code' indicates an error state
-	Debug null.String `json:"debug"`           // Debug information which may assist a programmer working with the API. Only included if 'code' indicates an error state
+	Debug null.String `json:"debug,omitzero"`  // Debug information which may assist a programmer working with the API. Only included if 'code' indicates an error state
 }
